749008bcc08bd04ed05dec5aea9070a0aa77427b
ambatib <30311196+ambatib@users.noreply.github.com>
2020-04-08 15:14:47 +0530

    Creating dynamic forms - FormArray

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -29,6 +30,9 @@ export class ProfileEditorComponent implements OnInit {
       state: [''],
       zip: ['']
     }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
   });
 
   constructor(private fb: FormBuilder) { }
@@ -48,6 +52,13 @@ export class ProfileEditorComponent implements OnInit {
         street: '1 Main Street'
       }
     });
+  }
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
   }
 
 }
